259423a14181a9d5f83af3c1ecce73f825832efe
Haesya <annamuhanova88@gmail.com>
2025-06-07 23:11:50 +0300

    start

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,19 @@
+import style from './footer.module.css'
+
+const RenderFooter = () => {
+
+
+    return (
+        <>
+            <div>Скан</div>
+            <div>
+                <span>г. Москва, Цветной б-р, 40</span>
+                <span>+7 495 771 21 11</span>
+                <span>info@skan.ru</span>
+                <span>Copyright, 2022</span>
+            </div>
+        </>
+    )
+}
+
+export {RenderFooter}
