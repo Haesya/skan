375e5a360d0439132de3a3ded21956ae81e6d848
Haesya <annamuhanova88@gmail.com>
2025-06-09 12:07:11 +0300

    header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import style from './header.module.css'
 import skan from '../../../public/skan.png'
+import burgerMenu from '../../../public/burger.svg'
 
 const RenderHeader = () => {
 
@@ -7,20 +8,29 @@ const RenderHeader = () => {
         <header>
             <div className={style.header__content}>
                 <div className={style.logo}><img src={skan} alt={'logoskan'}/></div>
-                <div>
+                <div className={style.sections}>
                     <ul>
                         <li>Главная</li>
                         <li>Тарифы</li>
                         <li>FAQ</li>
                     </ul>
                 </div>
-                <div className={style.using__companies}>
-                    <div>Использовано компаний:</div>
-                    <div>Лимит по компаниям:</div>
+                <div className={style.companies}>
+                    <div>
+                        <span>Использовано компаний:</span>
+                        <span className={style.using}>34</span>
+                    </div>
+                    <div>
+                        <span>Лимит по компаниям:</span>
+                        <span className={style.limit}>100</span>
+                    </div>
+                </div>
+                <div className={style.burger__menu}>
+                    <img src={burgerMenu} alt={'burgerMenu'}/>
                 </div>
                 <div className={style.authorization}>
-                    <div>Зарегистрироваться</div>
-                    <div className={style.stick}>Палка</div>
+                    <div className={style.register}>Зарегистрироваться</div>
+                    <div className={style.stick}></div>
                     <button className={style.login}>Войти</button>
                 </div>
                 <div className={style.user}>
