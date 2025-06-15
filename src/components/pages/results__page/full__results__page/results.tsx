c74f8fb4491f6709b98963c0daca00996eaddbdb
Haesya <annamuhanova88@gmail.com>
2025-06-15 23:00:07 +0300

    слайдер с результатами

--- src/components/pages/results__page/full__results__page/results.tsx
+++ src/components/pages/results__page/full__results__page/results.tsx
@@ -4,8 +4,47 @@ import {RenderFooter} from "../../../littleComponents/footer/footer.tsx";
 import tryToFind from '/tryToFind.png'
 import {RenderOnePublication} from "../one__publication/onePublication.tsx";
 import {RenderSummarySlider} from "../summary__slider/summarySlider.tsx";
+import {useDispatch, useSelector} from "react-redux";
+import type {RootState} from "@reduxjs/toolkit/query";
+import type {AppDispatch} from "../../../../store/store.ts";
+import {useEffect} from "react";
+import {useNavigate} from "react-router";
+import {setInitialShownDocs, showDocs} from "../../../../store/Slices/documentsReducer.ts";
+import {PostDocuments} from "../../../../store/documents.tsx";
 
 const RenderResults = () => {
+    const dispatch: AppDispatch = useDispatch();
+    const navigate = useNavigate();
+    const sliderData = useSelector((state: RootState) => state.histograms.data);
+    const documents = useSelector((state: RootState) => state.docs.data);
+    const shownDocs = useSelector((state: RootState) => state.docs.shownDocs);
+    const status = useSelector((state: RootState) => state.docs.status);
+    const error = useSelector((state: RootState) => state.docs.error);
+    const encodedIds = useSelector((state: RootState) => state.objectsearch.dataObjectsearch);
+    const isLoggedIn = useSelector((state: RootState) => !!state.auth.accessToken);
+
+    useEffect(() => {
+        if(!isLoggedIn) {
+            navigate('/')
+        }
+    }, [isLoggedIn, navigate]);
+
+    useEffect(() => {
+        dispatch(setInitialShownDocs(10)); // Установить показанные документы обратно на 10 при загрузке страницы
+    }, [dispatch]);
+
+    const allDocsDisplayed = shownDocs >= documents.length;
+
+    useEffect(() => {
+        if (encodedIds.length > 0) {
+            dispatch(PostDocuments({ listEncodedId: encodedIds }));
+        }
+    }, [encodedIds, dispatch]);
+
+    const LoadMore = () => {
+        dispatch(showDocs(10));
+    };
+
     return (
         <>
             <RenderHeader/>
@@ -20,7 +59,7 @@ const RenderResults = () => {
                 <div className={style.summary}>
                     <span className={style.big__text}>Общая сводка</span>
                     <span>Найдено вариантов</span>
-                    <RenderSummarySlider />
+                    <RenderSummarySlider sliderData={sliderData}/>
                 </div>
                 <span className={style.big__text}>СПИСОК ДОКУМЕНТОВ</span>
                 <div className={style.list__of__documents}>
@@ -28,7 +67,10 @@ const RenderResults = () => {
                         <RenderOnePublication />
                         <RenderOnePublication />
                     </div>
-                    <button className={style.load__more}>Показать больше</button>
+                    <button
+                        className={style.load__more}
+                        onClick={LoadMore}
+                    >Показать больше</button>
                 </div>
             </div>
             <RenderFooter/>
--- src/components/pages/results__page/summary__slider/summarySlider.tsx
+++ src/components/pages/results__page/summary__slider/summarySlider.tsx
@@ -1,10 +1,71 @@
 import style from './summarySlider.module.css'
+import arrowLeft from '/aleft.svg'
+import arrowRight from '/aright.svg'
+import {useState} from "react";
 
-const RenderSummarySlider = () => {
- return (
-     <>
-     </>
- )
+interface SliderData {
+    data: {
+        data: {
+            date: string;
+            value: number;
+        }[];
+        histogramType: string;
+    }[];
+}
+interface SliderProps {
+    sliderData: SliderData;
+}
+
+const RenderSummarySlider = ({sliderData}: SliderProps) => {
+    const [firstIndex, setFirstIndex] = useState(0)
+    const totalDocumentsData = sliderData.data.find(item => item.histogramType === 'totalDocuments')?.data || [];
+
+    const riskFactorsData = sliderData.data.find(
+        item => item.histogramType === 'riskFactors')?.data || [];
+
+    const CardsToShow = 8;
+    const visibleDocumentsData = totalDocumentsData.slice(firstIndex, firstIndex + CardsToShow)
+    console.log(visibleDocumentsData, 'visible')
+
+    const goLeft = () => {
+        setFirstIndex(prevIndex => Math.max(prevIndex - 1, 0));
+    }
+
+    const goRight = () => {
+        if (firstIndex + CardsToShow < totalDocumentsData.length) {
+            setFirstIndex(prevIndex => prevIndex + 1);
+        }
+    }
+
+    return (
+        <>
+            <div className={style.summary__slider}>
+                <button onClick={goLeft}><img src={arrowLeft} alt={'arrowLeft'}/></button>
+                <div className={style.slider}>
+                    <div className={style.slider__header}>
+                        <div>Период</div>
+                        <div>Всего</div>
+                        <div>Риски</div>
+                    </div>
+                    <div className={style.slider__line}>
+                        {
+                            visibleDocumentsData.map((item, index) => (
+                                <>
+                                    <div className={style.slider__block}>
+                                        <div>{new Date(item.date).toLocaleDateString()}</div>
+                                        <div>{item.value.toLocaleString()}</div>
+                                        <div>{riskFactorsData.toLocaleString() ? riskFactorsData[index].value.toLocaleString() : 0}</div>
+                                    </div>
+                                    <div className={style.slider__stick}></div>
+                                </>
+                            ))
+                        }
+                    </div>
+                </div>
+                <button onClick={goRight}><img src={arrowRight} alt={'arrowRight'}/></button>
+            </div>
+        </>
+    )
 }
 
 export {RenderSummarySlider}
